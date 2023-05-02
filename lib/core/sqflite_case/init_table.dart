bf07af81045625d1bd6c8bb184739f259267e27d
unknown <jeremimanogi@gmail.com>
2023-05-02 19:00:22 +0700

    create setup for db sqflite

---
+++ lib/core/sqflite_case/init_table.dart
@@ -0,0 +1,8 @@
+import 'package:sqflite/sqflite.dart';
+
+Future<void> initTables({
+  required Database db,
+  required int version,
+}) async {
+  
+}
---
+++ lib/core/sqflite_case/setup_db.dart
@@ -0,0 +1,27 @@
+import 'package:aplikasi_keuangan/core/sqflite_case/init_table.dart';
+import 'package:aplikasi_keuangan/core/sqflite_case/update_table.dart';
+import 'package:sqflite/sqflite.dart';
+
+const int sqfLiteDatabaseVersion = 1;
+const String sqfLiteDatabase = "database_aplikasi_keuangan";
+
+Future<Database> getAndSetupDB() async {
+    final db = await openDatabase(
+    sqfLiteDatabase,
+    version: sqfLiteDatabaseVersion,
+    onCreate: (db, version) async {
+      await initTables(
+        db: db,
+        version: version,
+      );
+    },
+    onUpgrade: (db, oldVersion, newVersion) async {
+      await updateTables(
+        db,
+        oldVersion,
+        newVersion,
+      );
+    },
+  );
+  return db;
+}
