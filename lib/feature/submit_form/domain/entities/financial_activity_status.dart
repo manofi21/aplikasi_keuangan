588dca046eefab46c6edee310065286837634b6e
unknown <jeremimanogi@gmail.com>
2023-05-03 18:39:52 +0700

    create model and enum for financial activity

---
+++ lib/feature/submit_form/domain/entities/financial_activity_status.dart
@@ -0,0 +1,30 @@
+enum StatusFinancialActivity {
+  income,
+  outcome
+}
+
+extension StatusFinancialActivityExt on StatusFinancialActivity {
+  int get asIndex {
+    switch (this) {
+      case StatusFinancialActivity.income:
+        return 1;
+      case StatusFinancialActivity.outcome:
+        return 2;
+      default:
+        return 1;
+    }
+  }
+}
+
+extension IntToStatusFinancialActivityExt on int {
+  StatusFinancialActivity get asIndex {
+    switch (this) {
+      case 1:
+        return StatusFinancialActivity.income;
+      case 2:
+        return StatusFinancialActivity.outcome;
+      default:
+        return StatusFinancialActivity.income;
+    }
+  }
+}
