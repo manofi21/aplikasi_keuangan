44490fa9ad2faebcfe1f3b20d41ee14ae1ebf967
unknown <jeremimanogi@gmail.com>
2023-05-11 23:13:32 +0700

    impl add financial repo impl abstract method

--- lib/feature/submit_form/data/repos/financial_activity_repo_impl.dart
+++ lib/feature/submit_form/data/repos/financial_activity_repo_impl.dart
@@ -1,11 +1,21 @@
+import 'package:aplikasi_keuangan/core/utils/sql_base/sql_base_repo.dart';
 import 'package:aplikasi_keuangan/feature/submit_form/domain/entities/financial_activity.dart';
 
 import '../../domain/repos/financial_activity_repo.dart';
+import '../model/financial_activiy_model.dart';
 
 class FinanancialActivityRepoImpl implements FinanancialActivityRepo {
   @override
-  Future<void> addFinancialActivity(FinancialActivity params) {
-    throw UnimplementedError();
+  Future<void> addFinancialActivity(FinancialActivity params) async {
+    final repoSql = SqlBaseRepoImpl();
+    final financialActivity = FinancialActivityModel(
+      memo: params.memo,
+      status: params.status,
+      amount: params.amount,
+      dateTime: params.dateTime.toString()
+    );
+    
+    return repoSql.insertData(FinancialActivityModel.table, financialActivity.toMap());
   }
 
   @override
