164089b33aaf4bf4f57e25b4acbf228289be0bcf
unknown <jeremimanogi@gmail.com>
2023-05-19 18:15:40 +0700

    create financial activity widget

---
+++ lib/feature/submit_form/utils/widget/list_financial_activity_widget.dart
@@ -0,0 +1,82 @@
+import 'package:aplikasi_keuangan/feature/financial_activity/domain/entities/financial_activity_status.dart';
+import 'package:aplikasi_keuangan/ui_export.dart';
+import 'package:intl/intl.dart';
+import '../../../../core/converter/currency_idr_string.dart';
+import '../../../financial_activity/domain/entities/financial_activity.dart';
+
+class ListFinancialActivityWidget extends StatefulWidget {
+  const ListFinancialActivityWidget({super.key});
+
+  @override
+  State<ListFinancialActivityWidget> createState() =>
+      _ListFinancialActivityWidgetState();
+}
+
+class _ListFinancialActivityWidgetState
+    extends State<ListFinancialActivityWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final listFinancialActivity = context.watch<List<FinancialActivity>>();
+    return ListView.separated(
+      itemCount: listFinancialActivity.length,
+      separatorBuilder: (context, index) {
+        return const SizedBox(height: 10);
+      },
+      itemBuilder: (context, index) {
+        return FinancialActivityItem(
+          financialActivity: listFinancialActivity[index],
+        );
+      },
+    );
+  }
+}
+
+class FinancialActivityItem extends StatelessWidget {
+  final FinancialActivity financialActivity;
+  const FinancialActivityItem({Key? key, required this.financialActivity})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget childItem(String title, String content) {
+      final theme = Theme.of(context);
+      return Row(children: [
+        Expanded(
+            flex: 2,
+            child: Text(
+              title,
+              style: theme.textTheme.bodyLarge,
+            )),
+        const Text(":"),
+        const SizedBox(
+          width: 15,
+        ),
+        Expanded(
+            flex: 2,
+            child: Text(
+              content,
+              style: theme.textTheme.bodyLarge,
+            )),
+      ]);
+    }
+
+    return Card(
+      margin: const EdgeInsets.only(left: 20, top: 10, bottom: 20, right: 20),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            childItem("Status", financialActivity.status.asStringTitle),
+            childItem(
+              "Date",
+              DateFormat('EEEE MMMM yyyy, h:mm a')
+                  .format(financialActivity.dateTime),
+            ),
+            childItem("Amount", currencyIdrString(financialActivity.amount)),
+            childItem("Memo", financialActivity.memo ?? ' - '),
+          ],
+        ),
+      ),
+    );
+  }
+}
