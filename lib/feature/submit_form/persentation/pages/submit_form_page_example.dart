3c49a72506751c552b6d6200533e31ba4896425d
unknown <jeremimanogi@gmail.com>
2023-05-19 18:16:32 +0700

    impl function for disable/enable field, and save function

--- lib/feature/submit_form/persentation/pages/submit_form_page_example.dart
+++ lib/feature/submit_form/persentation/pages/submit_form_page_example.dart
@@ -1,7 +1,12 @@
-import 'package:aplikasi_keuangan/feature/submit_form/persentation/widget/field_financial_currency.dart';
-import 'package:aplikasi_keuangan/feature/submit_form/persentation/widget/field_memo.dart';
-import 'package:aplikasi_keuangan/feature/submit_form/persentation/widget/save_button.dart';
-import 'package:flutter/material.dart';
+import 'package:aplikasi_keuangan/feature/submit_form/utils/function/determine_financial_activty_type.dart';
+import 'package:aplikasi_keuangan/feature/submit_form/utils/widget/field_financial_currency.dart';
+import 'package:aplikasi_keuangan/feature/submit_form/utils/widget/field_memo.dart';
+import 'package:aplikasi_keuangan/feature/submit_form/utils/widget/save_button.dart';
+import 'package:aplikasi_keuangan/ui_export.dart';
+
+import '../../../financial_activity/domain/entities/financial_activity_status.dart';
+import '../../utils/widget/list_financial_activity_widget.dart';
+import '../provider/submit_form_provider.dart';
 
 class SubmitFormPage extends StatefulWidget {
   const SubmitFormPage({super.key});
@@ -14,9 +19,52 @@ class _SubmitFormPageState extends State<SubmitFormPage> {
   final financialIncomeTextController = TextEditingController();
   final financialOutcomeTextController = TextEditingController();
   final noteTextController = TextEditingController();
+  StatusFinancialActivity currentStatus = StatusFinancialActivity.init;
+  String amountInput = "";
+
+  void onAfterClickSave() {
+    setState(() {
+      financialIncomeTextController.clear();
+      financialOutcomeTextController.clear();
+      noteTextController.clear();
+      currentStatus = StatusFinancialActivity.init;
+      amountInput = "";
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((Duration duration) {
+      financialIncomeTextController.addListener(() {
+        setState(() {
+          currentStatus = checkStatusActivity(
+            financialIncomeTextController.text.isNotEmpty,
+            financialOutcomeTextController.text.isNotEmpty,
+          );
+        });
+        amountInput = financialIncomeTextController.text;
+      });
+
+      financialOutcomeTextController.addListener(() {
+        setState(() {
+          currentStatus = checkStatusActivity(
+            financialIncomeTextController.text.isNotEmpty,
+            financialOutcomeTextController.text.isNotEmpty,
+          );
+        });
+        currentStatus = checkStatusActivity(
+          financialIncomeTextController.text.isNotEmpty,
+          financialOutcomeTextController.text.isNotEmpty,
+        );
+        amountInput = financialOutcomeTextController.text;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final submitForm = context.read<SubmitFormProvider>();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Financial Activity"),
@@ -27,22 +75,38 @@ class _SubmitFormPageState extends State<SubmitFormPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-            const SizedBox(height: 10),
+              const SizedBox(height: 10),
               FieldFinancialCurrency(
                 labelText: "Pemasukan/Income",
                 controller: financialIncomeTextController,
+                isFiedlEnabled: currentStatus.isStatusFieldIncome,
               ),
               const SizedBox(height: 10),
               FieldFinancialCurrency(
                 labelText: "Pengeluaran/Outcome",
                 controller: financialOutcomeTextController,
+                isFiedlEnabled: currentStatus.isStatusFieldOutcome,
               ),
               const SizedBox(height: 10),
               FieldMemo(controller: noteTextController),
               SaveButton(
                 onPressed: () {
-                  
+                  submitForm
+                      .addActivity(
+                          amount: amountInput,
+                          status: currentStatus,
+                          memo: noteTextController.text.isEmpty
+                              ? null
+                              : noteTextController.text)
+                      .whenComplete(
+                    () {
+                      onAfterClickSave();
+                    },
+                  );
                 },
+              ),
+              const Expanded(
+                child: ListFinancialActivityWidget(),
               )
             ],
           ),
