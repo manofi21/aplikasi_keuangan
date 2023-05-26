bca3d5b872d3698715a19d6c706fbf20f3262bc1
unknown <jeremimanogi@gmail.com>
2023-05-26 08:32:14 +0700

    adding equitable and impl in financial actiity

--- lib/feature/financial_activity/domain/entities/financial_activity.dart
+++ lib/feature/financial_activity/domain/entities/financial_activity.dart
@@ -1,17 +1,25 @@
+import 'package:equatable/equatable.dart';
+
 import 'financial_activity_status.dart';
 
-class FinancialActivity {
+class FinancialActivity extends Equatable {
   final String? memo;
   final int amount;
   final StatusFinancialActivity status;
   final DateTime dateTime;
   final String category;
 
-  FinancialActivity({
+  const FinancialActivity({
     this.memo,
     required this.amount,
     required this.status,
     required this.dateTime,
     required this.category,
   });
+
+  @override
+  List<Object?> get props => [memo, amount, status, dateTime, category];
+
+  @override
+  bool get stringify => true;
 }
